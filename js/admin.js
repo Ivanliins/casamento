13da84f8dfb65a9e10cdc7a00a885df7f546d576
Clube do Med <contato@clubedomed.com.br>
2026-09-06 17:03:36 -0300

    fix(admin): remove limpeza automatica que resetava novos envios no celular

--- js/admin.js
+++ js/admin.js
@@ -42,12 +42,6 @@ function initAdminAuth() {
     sessionStorage.setItem("admin_logged", "true");
     localStorage.setItem("admin_logged", "true");
 
-    // Limpa a lista para o novo teste solicitado pelos noivos
-    if (localStorage.getItem("admin_cleaned_v3") !== "true") {
-      window.weddingDB.clearAllRSVPs();
-      localStorage.setItem("admin_cleaned_v3", "true");
-    }
-
     loadAdminData();
     startAutoSync();
   }
